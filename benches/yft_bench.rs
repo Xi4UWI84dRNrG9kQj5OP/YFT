50a052b94926825c811a83f3bb6e445848397f66
Das Ende naht <31@03.2017>
2019-09-13 14:13:21 +0200

    ~ bench clean up

--- benches/yft_bench.rs
+++ benches/yft_bench.rs
@@ -8,28 +8,67 @@ use criterion::Criterion;
 //use criterion::black_box;
 use criterion::BenchmarkId;
 use criterion::Throughput;
+use criterion::measurement::WallTime;
 use std::time::Duration;
 use uint::u40;
 
 pub fn criterion_benchmark(c: &mut Criterion) { //TODO das muss auch mit parametern gehen!
 //    let values = yft::nmbrsrc::get_uniform_dist(10000000);
     let values = yft::nmbrsrc::load("C:\\tmp\\i.dat").unwrap();
-//    let yft = yft::yft40::YFT::new(values.into_iter().map(|v| u40::from(v)).collect(), &mut None, 8, 10);
-    let yft = yft::yft::YFT::new(values, &mut None, 8, 10);
+//    let queries = yft::nmbrsrc::get_uniform_dist(5);
+    let queries = yft::nmbrsrc::load("C:\\tmp\\t.dat").unwrap();
 
+
+//    bench_bin(c, &values, queries);
+    bench_yft(c, values, queries);
+//    bench_yft40(c, values, queries);
+}
+
+fn bench_yft(c: &mut Criterion<WallTime>, values: Vec<usize>, queries: Vec<usize>) {
+    let yft = yft::yft::YFT::new(values, &mut None, 8, 10);
     let mut group = c.benchmark_group("query");
-    for s in yft::nmbrsrc::get_uniform_dist(5).iter() {
+    for s in queries.iter() {
         group.throughput(Throughput::Bytes(*s as u64));
 //        group.sample_size(10);
         group.measurement_time(Duration::from_millis(100));
         group.warm_up_time(Duration::from_millis(100));
         group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-//            b.iter(|| yft.predecessor(u40::from(s)));
             b.iter(|| yft.predecessor(s));
         });
+    }
+    group.finish();
+}
+fn bench_yft40(c: &mut Criterion<WallTime>, values: Vec<usize>, queries: Vec<usize>) {
+    let yft = yft::yft40::YFT::new(values.into_iter().map(|v| u40::from(v)).collect(), &mut None, 8, 10);
+    let mut group = c.benchmark_group("query");
+    for s in queries.iter() {
+        group.throughput(Throughput::Bytes(*s as u64));
+//        group.sample_size(10);
+        group.measurement_time(Duration::from_millis(100));
+        group.warm_up_time(Duration::from_millis(100));
+        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
+            b.iter(|| yft.predecessor(u40::from(s)));
+        });
+    }
+    group.finish();
+}
+
+fn bench_bin(c: &mut Criterion<WallTime>, values: &Vec<usize>, queries: Vec<usize>) {
+//    let yft = yft::yft40::YFT::new(values.into_iter().map(|v| u40::from(v)).collect(), &mut None, 8, 10);
+//    let yft = yft::yft::YFT::new(values, &mut None, 8, 10);
+    let mut group = c.benchmark_group("query");
+    for s in queries.iter() {
+//        group.throughput(Throughput::Bytes(*s as u64));
+////        group.sample_size(10);
+//        group.measurement_time(Duration::from_millis(100));
+//        group.warm_up_time(Duration::from_millis(100));
 //        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-//            b.iter(|| pred(&values,s));
+////            b.iter(|| yft.predecessor(u40::from(s)));
+//            b.iter(|| yft.predecessor(s));
 //        });
+        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
+            b.iter(|| pred(&values, s));
+        });
     }
     group.finish();
 }
