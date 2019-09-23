077a827ef46b578418b714f418ec381e48ca9608
Das Ende naht <31@03.2017>
2019-09-23 17:41:52 +0200

    + option to test with decreasing input length

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,9 @@ struct Args {
     /// Minimal height of lowest lss level
     #[structopt(short = "a", long, default_value = "10")]
     min_start_level: usize,
+    /// Run multiple times, each time with half much elements than before
+    #[structopt(short, long)]
+    element_length_test: bool,
     /// A file with ordered Numbers to create the Y-Fast-Trie
     #[structopt(short, long, parse(from_os_str))]
     load: Option<PathBuf>,
@@ -48,21 +51,21 @@ struct Args {
     /// Print yft to outline
     #[structopt(short, long)]
     print: bool,
+    /// File with predecessor queries
+    #[structopt(short, long, parse(from_os_str))]
+    queries: Option<PathBuf>,
     /// A file where randomly generated Values from this run should be saved to
     #[structopt(short, long, parse(from_os_str))]
     store: Option<PathBuf>,
     /// Log time
     #[structopt(short, long)]
     time: bool,
-    /// File with predecessor queries
-    #[structopt(short, long, parse(from_os_str))]
-    queries: Option<PathBuf>,
-    /// Maximum number of lss levels
-    #[structopt(short = "z", long, default_value = "8")]
-    max_lss_level: usize,
     /// Use 40 bit integer
     #[structopt(short, long)]
     u40: bool,
+    /// Maximum number of lss levels
+    #[structopt(short = "z", long, default_value = "8")]
+    max_lss_level: usize,
 }
 
 // arg subcommand for number generation
@@ -79,8 +82,8 @@ enum Distribution {
     },
     Poisson {
         length: usize,
-        lambda: f64
-    }
+        lambda: f64,
+    },
 }
 
 
@@ -104,81 +107,101 @@ fn main() {
         log.set_log_time();
     };
 
-    //create yft input
-    let values =
-        if let Some(file) = args.load {
-            nmbrsrc::load(file.to_str().unwrap()).unwrap()
+
+    for i in 0..40 { //for element length test, else ignored //TODO change log
+        //create yft input
+        let mut values =
+            if let Some(ref file) = args.load {
+                nmbrsrc::load(file.to_str().unwrap()).unwrap()
+            } else {
+                if let Some(ref distribution) = args.dist {
+                    match distribution {
+                        Distribution::Normal { length, mean, deviation } => {
+                            nmbrsrc::get_normal_dist(*length, *mean as f64, *deviation as f64)
+                        }
+                        Distribution::Uniform { length } => {
+                            nmbrsrc::get_uniform_dist(*length)
+                        }
+                        Distribution::Poisson { length, lambda } => {
+                            nmbrsrc::get_poisson_dist(*length, *lambda)
+                        }
+                    }
+                } else {
+                    panic!("Distribution or input File required!");
+                }
+            };
+
+
+        //save input if option is set
+        if let Some(ref file) = args.store {
+            if let Err(e) = nmbrsrc::save(&values, file.to_str().unwrap()) {
+                dbg!(e);
+            }
+        }
+
+        if args.element_length_test && i > 0 {
+            //half number of elements
+            values = values.iter().step_by(2usize.pow(i)).map(|v| v.clone()).collect();
+            if values.len() < 2 {
+                break;
+            }
         } else {
-            if let Some(distribution) = args.dist {
-                match distribution {
-                    Distribution::Normal { length, mean, deviation } => {
-                        nmbrsrc::get_normal_dist(length, mean as f64, deviation as f64)
+            if i > 1 {
+                break;
+            }
+        }
+
+        log.log_mem("values loaded").log_time("values loaded");
+
+        {
+            //create yft
+            if args.u40 {
+                let yft = yft40::YFT::new(values.into_iter().map(|v| u40::from(v)).collect(), args.min_start_level, args.max_lss_level, &mut log);
+
+                log.log_mem("yft initialized").log_time("yft initialized");
+
+                //load queries & aply them, if option is set
+                if let Some(ref file) = args.queries {
+                    let test_values = nmbrsrc::load(file.to_str().unwrap()).unwrap();
+                    if let Some(ref output) = args.output {
+                        let predecessors = &test_values.into_iter().map(|v| usize::from(yft.predecessor(u40::from(v)).unwrap_or(u40::from(0)))).collect();
+                        if let Err(e) = nmbrsrc::save(predecessors, output.to_str().unwrap()) {
+                            dbg!(e);
+                        }
+                    } else {
+                        let _: Vec<usize> = test_values.into_iter().map(|v| usize::from(yft.predecessor(u40::from(v)).unwrap_or(u40::from(0)))).collect(); //TODO time
                     }
-                    Distribution::Uniform { length } => {
-                        nmbrsrc::get_uniform_dist(length)
-                    }
-                    Distribution::Poisson { length, lambda } => {
-                        nmbrsrc::get_poisson_dist(length, lambda)
-                    }
+                    log.log_time("queries processed");
+                }
+                if args.memory {
+                    yft.print_stats(&log);
                 }
             } else {
-                panic!("Distribution or input File required!");
-            }
-        };
-    //save input if option is set
-    if let Some(file) = args.store {
-        if let Err(e) = nmbrsrc::save(&values, file.to_str().unwrap()) {
-            dbg!(e);
+                let yft = YFT::new(values, args.min_start_level, args.max_lss_level, &mut log);
+
+                log.log_mem("yft initialized").log_time("yft initialized");
+
+                //load queries & aply them, if option is set
+                if let Some(ref file) = args.queries {
+                    let test_values = nmbrsrc::load(file.to_str().unwrap()).unwrap();
+                    if let Some(ref output) = args.output {
+                        let predecessors = &test_values.into_iter().map(|v| yft.predecessor(v).unwrap_or(0)).collect();
+                        if let Err(e) = nmbrsrc::save(predecessors, output.to_str().unwrap()) {
+                            dbg!(e);
+                        }
+                    } else {
+                        let _: Vec<usize> = test_values.into_iter().map(|v| yft.predecessor(v).unwrap_or(0)).collect();
+                    }
+                    log.log_time("queries processed");
+                }
+                if args.memory {
+                    yft.print_stats(&log);
+                }
+            };
+            //yft mem is freed here
         }
     }
-
-    log.log_mem("values loaded").log_time("values loaded");
-    {
-        //create yft
-        if args.u40 {
-            let yft = yft40::YFT::new(values.into_iter().map(|v| u40::from(v)).collect(), args.min_start_level, args.max_lss_level, &mut log);
-
-            log.log_mem("yft initialized").log_time("yft initialized");
-
-            //load queries & aply them, if option is set
-            if let Some(file) = args.queries {
-                let test_values = nmbrsrc::load(file.to_str().unwrap()).unwrap();
-                if let Some(output) = args.output {
-                    let predecessors = &test_values.into_iter().map(|v| usize::from(yft.predecessor(u40::from(v)).unwrap_or(u40::from(0)))).collect();
-                    if let Err(e) = nmbrsrc::save(predecessors, output.to_str().unwrap()) {
-                        dbg!(e);
-                    }
-                } else {
-                    let _: Vec<usize> = test_values.into_iter().map(|v| usize::from(yft.predecessor(u40::from(v)).unwrap_or(u40::from(0)))).collect(); //TODO time
-                }
-                log.log_time("queries processed");
-            }
-            if args.memory {
-                yft.print_stats(&log);
-            }
-        } else {
-            let yft = YFT::new(values, args.min_start_level, args.max_lss_level, &mut log);
-
-            log.log_mem("yft initialized").log_time("yft initialized");
-
-            //load queries & aply them, if option is set
-            if let Some(file) = args.queries {
-                let test_values = nmbrsrc::load(file.to_str().unwrap()).unwrap();
-                if let Some(output) = args.output {
-                    let predecessors = &test_values.into_iter().map(|v| yft.predecessor(v).unwrap_or(0)).collect();
-                    if let Err(e) = nmbrsrc::save(predecessors, output.to_str().unwrap()) {
-                        dbg!(e);
-                    }
-                } else {
-                    let _: Vec<usize> = test_values.into_iter().map(|v| yft.predecessor(v).unwrap_or(0)).collect();
-                }
-                log.log_time("queries processed");
-            }
-            if args.memory {
-                yft.print_stats(&log);
-            }
-        };
-    }
+    {} // end for
     log.log_mem("end");
 
 //    if let Some(file) = args.gnuplot {
